7a2aeda477f6b9c6ad87d1ef3761c0fdba3ef68e
CathiR <catharinariedel@freenet.de>
2026-04-23 12:08:26 +0200

    Trainingsplan drucken8

--- src/App.jsx
+++ src/App.jsx
@@ -482,7 +482,7 @@ export default function App() {
   <div class="contact-block">
     <div class="contact-item"><svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="#3D8E8F" stroke-width="2.5" stroke-linecap="round"><circle cx="12" cy="12" r="10"/><line x1="2" y1="12" x2="22" y2="12"/><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"/></svg><a href="https://www.fit-fun-dog.de" target="_blank" style="color:#555;text-decoration:none">www.fit-fun-dog.de</a></div>
     <div class="contact-item"><svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="#3D8E8F" stroke-width="2.5" stroke-linecap="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg><a href="mailto:info@fit-fun-dog.de" style="color:#555;text-decoration:none">info@fit-fun-dog.de</a></div>
-    <div class="contact-item"><svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="#3D8E8F" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></svg>0159 / 04976681</div>
+    <div class="contact-item"><svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="#3D8E8F" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></svg><span style="color:#555;text-decoration:none">0159 / 04976681</span></div>
   </div>
   <div class="patient-block">
     <div class="patient-label">P&nbsp;A&nbsp;T&nbsp;I&nbsp;E&nbsp;N&nbsp;T</div>
@@ -555,7 +555,7 @@ ${patExercises.map((ex) => `
 </body></html>`;
 
     // Open new window and write HTML directly - avoids blob: URL in print header
-    const printWindow = window.open("", "_blank");
+    const printWindow = window.open("about:blank", "_blank");
     if (printWindow) {
       printWindow.document.open();
       printWindow.document.write(html);
